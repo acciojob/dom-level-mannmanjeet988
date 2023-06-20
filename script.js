73200838aee2cb9aab7408042a35c958dcb2a6a3
mannmanjeet988 <120735535+mannmanjeet988@users.noreply.github.com>
2023-06-20 13:18:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+const liElements = document.getElementsByTagName("li")
+for(let i =0;i<liElements.length;i++){
+	if(liElements[i].id === level)
+		alert(The level of the element is: i+1);
+}
